01ea29c888cd9a93f0069a1a53a35646c8c4e82c
Axel Engström <axel@axelca.com>
2020-03-02 22:40:56 +0100

    add texts

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -47,9 +47,12 @@ const Summary = ({ data, loading }) => {
         <Divider />
         <div className={classes.root}>
           <Grid container spacing={5}>
-            <Grid item xs={12} md={2}>
+            <Grid item xs={12} md={3}>
               <Paper className={classes.paper}>
                 <Typography variant="h6">Medelålder</Typography>
+                <Typography variant="caption">
+                  Snittålder i nuvarande urval
+                </Typography>
                 <Typography
                   variant="h4"
                   component="p"
@@ -59,35 +62,12 @@ const Summary = ({ data, loading }) => {
                 </Typography>
               </Paper>
             </Grid>
-            <Grid item xs={12} md={5}>
-              <Paper className={classes.paper}>
-                <Typography variant="h6">Partitillhörighet</Typography>
-                <Doughnut
-                  aria-label="Partitillhörighet"
-                  options={{
-                    layout: {
-                      padding: 20
-                    },
-                    events: ["mousemove", "mouseout", "touchstart", "touchmove"]
-                  }}
-                  data={{
-                    labels: partyDistribution.parties,
-                    datasets: [
-                      {
-                        label: "Partitillhörighet",
-                        data: partyDistribution.values,
-                        backgroundColor: partyDistribution.colors,
-                        borderColor: partyDistribution.colors,
-                        borderWidth: 1
-                      }
-                    ]
-                  }}
-                />
-              </Paper>
-            </Grid>
-            <Grid item xs={12} md={5}>
+            <Grid item xs={12} md={4}>
               <Paper className={classes.paper}>
                 <Typography variant="h6">Könsfördelning</Typography>
+                <Typography variant="caption">
+                  Antal män och kvinnor i nuvarande urval
+                </Typography>
                 <Pie
                   aria-label="Könsfördelning"
                   options={{
@@ -106,6 +86,35 @@ const Summary = ({ data, loading }) => {
                         data: genderDistribution.values,
                         backgroundColor: genderDistribution.colors,
                         borderColor: genderDistribution.colors,
+                        borderWidth: 1
+                      }
+                    ]
+                  }}
+                />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={5}>
+              <Paper className={classes.paper}>
+                <Typography variant="h6">Partitillhörighet</Typography>
+                <Typography variant="caption">
+                  Antal mandat per parti baserat på nuvarande urval
+                </Typography>
+                <Doughnut
+                  aria-label="Partitillhörighet"
+                  options={{
+                    layout: {
+                      padding: 20
+                    },
+                    events: ["mousemove", "mouseout", "touchstart", "touchmove"]
+                  }}
+                  data={{
+                    labels: partyDistribution.parties,
+                    datasets: [
+                      {
+                        label: "Partitillhörighet",
+                        data: partyDistribution.values,
+                        backgroundColor: partyDistribution.colors,
+                        borderColor: partyDistribution.colors,
                         borderWidth: 1
                       }
                     ]
@@ -134,12 +143,12 @@ const Summary = ({ data, loading }) => {
             </Grid>
             <Grid item xs={12} md={5}>
               <Paper className={classes.placeholder}>
-                <Typography variant="h6">Partitillhörighet</Typography>
+                <Typography variant="h6">Könsfördelning</Typography>
               </Paper>
             </Grid>
             <Grid item xs={12} md={5}>
               <Paper className={classes.placeholder}>
-                <Typography variant="h6">Könsfördelning</Typography>
+                <Typography variant="h6">Partitillhörighet</Typography>
               </Paper>
             </Grid>
           </Grid>
