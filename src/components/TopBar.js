ae2d8785f8c73baedfd5f36b1e249d2cb2cd7de6
Axel Engström <axel@axelca.com>
2020-03-02 22:40:45 +0100

    hide topbar on scroll

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Slide from "@material-ui/core/Slide";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import FilterListOutlinedIcon from "@material-ui/icons/FilterListOutlined";
-import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Container,
+  Typography
+} from "@material-ui/core";
+
+import logoInv from "../icons/logo-inv.svg";
 
 const useStyles = makeStyles(theme => ({
-  root: { background: "#413677" },
+  appBar: { background: "#413677" },
+  toolbar: { padding: 0 },
+  logo: { maxHeight: 24, marginRight: 20 },
   filterButton: {
     marginRight: theme.spacing(5),
     [theme.breakpoints.up("md")]: {
@@ -13,25 +25,45 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const HideOnScroll = props => {
+  const { children } = props;
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+};
+
 const TopBar = ({ handleDrawerToggle }) => {
   const classes = useStyles();
   return (
-    <AppBar position="fixed" className={classes.root}>
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="Öppna filter"
-          edge="start"
-          onClick={handleDrawerToggle}
-          className={classes.filterButton}
-        >
-          <FilterListOutlinedIcon />
-        </IconButton>
-        <Typography variant="h6" component="h1" noWrap>
-          Riksdagskollen
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <HideOnScroll>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Container>
+          <Toolbar className={classes.toolbar}>
+            <IconButton
+              color="inherit"
+              aria-label="Öppna filter"
+              edge="start"
+              onClick={handleDrawerToggle}
+              className={classes.filterButton}
+            >
+              <FilterListOutlinedIcon />
+            </IconButton>
+            <img
+              src={logoInv}
+              alt="Riksdagskollen logo"
+              className={classes.logo}
+            />
+            <Typography variant="h6" component="h1" noWrap>
+              Riksdagskollen
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </HideOnScroll>
   );
 };
 
